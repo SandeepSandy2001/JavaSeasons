ce68bdab807c0dc4c9c1607dcdade395f6202652
SandeepSandy2001 <bavusandeep000@gmail.com>
2025-12-14 14:30:57 +0530

    Character program is added

---
+++ src/stringMethodPrograms/StringCharMethod.java
@@ -0,0 +1,26 @@
+package stringMethodPrograms;
+
+public class StringCharMethod {
+
+	public static void main(String[] args) 
+	{
+		String s = "Hello";
+		
+		System.out.println(s.length());
+		
+		for(int i=0;i<s.length();i++)
+		{
+			System.out.println(s.charAt(i));
+		}
+		
+		char[] ch=s.toCharArray();
+		
+		System.out.println(ch.length);
+		
+		for(int i=0;i<ch.length;i++)
+		{
+			System.out.println(ch[i]);
+		}
+	}
+
+}
